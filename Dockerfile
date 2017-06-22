c6aeff6abad3b98fab4ab0340e17b8d5c2181b92
Opsolutely Autodeployer <github@jackcanty.com>
2017-06-22 08:48:01 -0700

    A running app

---
+++ Dockerfile
@@ -0,0 +1,22 @@
+FROM alpine
+
+COPY requirements.txt /tmp/requirements.txt
+RUN apk add --no-cache --update openssh git python3
+RUN pip3 install --upgrade pip setuptools
+RUN pip3 install -r /tmp/requirements.txt
+
+ENV APP_DIR /app
+ENV FLASK_APP app.py
+ENV GIT_AUTHOR_NAME "Opsolutely Autodeployer"
+ENV GIT_AUTHOR_EMAIL "autodeployer@opsolutely.com"
+
+
+# The CWD needs to be mounted at /app at run time
+WORKDIR ${APP_DIR}
+VOLUME ${APP_DIR}
+EXPOSE 5000
+
+# Cleanup
+RUN rm -rf /.wh /root/.cache /var/cache /tmp/requirements.txt
+
+CMD /usr/bin/flask run -h  0.0.0.0 -p 5000
--- app.py
+++ app.py
@@ -1,16 +1,18 @@
 import subprocess
 import json
-import time
+from time import time
 from flask import Flask
 
 process = subprocess.Popen(["git", "rev-parse", "HEAD"], stdout=subprocess.PIPE)
 sha = process.communicate()[0].replace(b'\n', b'').decode('utf-8')
 app = Flask(__name__)
-boot_time = time.time()
+app.new_commit_added = False
+boot_time = time()
 
 
 @app.route('/_status')
 def version():
+    maybe_add_commit()
     return json.dumps({
             'sha': sha,
             'deploy_time': boot_time,
@@ -20,3 +22,21 @@ def version():
 @app.route('/')
 def root():
     return '<h2>Current Autodeployer version: {}</h2>'.format(sha)
+
+
+def maybe_add_commit():
+    """
+    If the app has been booted for at least a minute then make a new git
+    commit, then no longer attempt this function for the life of the
+    process.
+    """
+    if app.new_commit_added:
+        return
+    if boot_time + 2 < time():
+        add_commit()
+        app.new_commit_added = True
+
+
+def add_commit():
+    subprocess.Popen(["git", "commit", "--allow-empty", "-m", "time()"], stdout=subprocess.PIPE)
+    subprocess.Popen(["git", "push", "origin", "master"], stdout=subprocess.PIPE)
