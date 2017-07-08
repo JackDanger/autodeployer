f1dd70a705680199e7afea3983ff21462231a71e
Opsolutely Autodeployer <github@jackcanty.com>
2017-07-07 20:38:53 -0700

    clarifying age on home page

--- app.py
+++ app.py
@@ -97,8 +97,8 @@ def root():
         <pre><code>{}</code></pre>
         <p>
           Alerting is programmed to read the <a href="/_age">/_age</a>
-          endpoint which will return HTTP 500 after this app has been
-          running for 60 seconds.
+          which will author and push a new git commit after the app has been
+          running for 60 seconds and will return HTTP 500 after ten minutes.
         </p>
     """.format(status())
 
